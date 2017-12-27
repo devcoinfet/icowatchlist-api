97324ab1795b9348ee9b2ab685811d263efdab26
devcoinfet <34756993+devcoinfet@users.noreply.github.com>
2017-12-27 11:42:59 -0800

    major update that will work with 2 dimensional array
    this will allow the data for each ico to be stored together ill reupload the rest on finishing I figured I'd update for anyone that wants both data on each pass to be a 1:1 correlation

--- api-example.py
+++ api-example.py
@@ -3,7 +3,9 @@ import json
 import argparse
 import sys
 import time
-
+from collections import defaultdict
+result_dic = defaultdict(dict)
+result_dic['name']['url']= [0][0]
 
 
 def get_args():
@@ -12,59 +14,27 @@ def get_args():
     parser = argparse.ArgumentParser(
         description='Script to Enumerate ICO\'s')
     # Add arguments
-    parser.add_argument(
-        '-f', '--finished', type=str, help='return a set of tuple\'s  from finished ICO\'s', required=False)
-    parser.add_argument(
-        '-u', '--upcoming', type=str, help='return a set of tuple\'s from upcoming ICO\'s', required=False, nargs='+')
+
+
     parser.add_argument(
         '-l', '--live', type=str, help='return a set of tuple\'s from live ICO\'s', required=False, default=None)
       # Array for all arguments passed to script
     args = parser.parse_args()
     # Assign args to variables
-    finished = args.finished
-    upcoming = args.upcoming
     live = args.live
     # Return all variable values
-    return finished, upcoming,live
+    return live
 
 def parser():
     url_live = 'https://api.icowatchlist.com/public/v1/live'
-    url_upcoming = 'https://api.icowatchlist.com/public/v1/upcoming'
-    url_finished = 'https://api.icowatchlist.com/public/v1/finished'
-    finished, upcoming, live = get_args()
-     
-    if finished:
-        print('finished ico list selected')
-        method = 'finished'
-        finished_names,finished_links = Api_Call(url_finished,method)
-        for live in finished_names:
-            print(live)
-
-        for live in finished_links:
-            print(live)
-        return finished_names,finished_links
-
-    if upcoming:
-        print('upcoming ico list selected')
-        method = 'upcoming'
-        upcoming_names,upcoming_links = Api_Call(url_upcoming,method)
-        for live in upcoming_names:
-            print(live)
-
-        for live in upcoming_links:
-            print(live)
-        return upcoming_names,upcoming_links
+    live = get_args()
        
     if live:
         print('live ico list selected')
         method = 'live'
-        live_names,live_links = Api_Call(url_live,method)
-        for live in live_names:
-            print(live)
+        Api_Call(url_live,method)
+        
 
-        for live in live_links:
-            print(live)
-        return live_names,live_links
 
 def Api_Call(url,method):
     try:
@@ -77,40 +47,18 @@ def Api_Call(url,method):
           for data in data_parsed:
               tmp_names.append(data['name'])
               tmp_lnks.append(data['website_link'])
-          return tmp_names,tmp_lnks
-        
-       elif method == "finished":
-          r = requests.get(url)
-          data = json.loads(r.content.decode('utf-8'))
-          data_parsed = data['ico']['finished']
-          for data in data_parsed:
-              tmp_names.append(data['name'])
-              tmp_lnks.append(data['website_link'])
-          return tmp_names,tmp_lnks
-
-       elif method == "upcoming":
-          r = requests.get(url)
-          data = json.loads(r.content.decode('utf-8'))
-          data_parsed = data['ico']['upcoming']
-          for data in data_parsed:
-              tmp_names.append(data['name'])
-              tmp_lnks.append(data['website_link'])
-          return tmp_names,tmp_lnks
-           
+              result_dic[0][1] = data['name'],data['website_link']
+              print(result_dic[0][1])
+             
+              
     except:
         pass
-    
+        
 
 def main():
-    parsed_names,parsed_urls = parser()
-    
-    
-    for domains in parsed_urls:
-        try:
-           print(domains)
-           
-        except:
-            pass
+    parser()
+         
+   
 
 if __name__ == "__main__":
     main()
